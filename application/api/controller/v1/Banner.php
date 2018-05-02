9be36b4829502daff69ce02d77514da51afd6387
wangqiang <wangqiangwen@163.com>
2018-05-02 17:54:31 +0800

    错误日志记录

--- application/api/controller/v1/Banner.php
+++ application/api/controller/v1/Banner.php
@@ -27,7 +27,7 @@ class Banner
         (new IDMustBePostivenInt())->goCheck();
         $banner = BannerModel::getBannerByID($id);
         if(!$banner){
-            throw new BannerMissException();
+            throw new Exception('内部错误');
         }
         return $banner;
 
